222635e6006cf03dee8c0e7f12ab4d4d045f61c1
Alex Pereira <alex.pereira@outlook.com>
2018-10-10 16:14:35 -0400

    Update package - add tests

--- src/factory/code/common/package.ts
+++ src/factory/code/common/package.ts
@@ -5,16 +5,15 @@ o---------COMMON   "version": "1.0.0",
 o---------COMMON   "description": "",
 o---------COMMON   "main": "server.js",
 o---------COMMON   "scripts": {
-o---------COMMON     "test": "echo \\"Error: no test specified\\" && exit 1",
 o-----------KNEX     "migrate": "node_modules/.bin/knex migrate:latest --knexfile database/knexfile.js",
 o-----------KNEX     "rollback": "node_modules/.bin/knex migrate:rollback --knexfile database/knexfile.js",
 o-----------KNEX     "seed": "node_modules/.bin/knex seed:run --knexfile database/knexfile.js",
+o----------MOCHA     "test": "node_modules/.bin/mocha tests --recursive --exit",
 o---------COMMON     "meeseeks": "echo \\"I\'m Mr. Meeseeks! Look at me!\\""
 o---------COMMON   },
 o---------COMMON   "author": "",
 o---------COMMON   "license": "ISC",
 o---------COMMON   "dependencies": {
-o---------COMMON     "dotenv": "*",
 o--------EXPRESS     "express": "*",
 o-----------HAPI     "hapi": "*",
 o----------MYSQL     "mysql": "*",
@@ -25,12 +24,14 @@ o-------MONGOOSE     "mongoose": "*",
 o------------JWT     "jsonwebtoken": "*",
 o-----EX-SESSION     "express-session": "*",
 o------------YAR     "yar": "*",
-o------------JOI     "joi": "*"
-o------VALIDATOR     "validator": "*"
+o------------JOI     "joi": "*",
+o------VALIDATOR     "validator": "*",
+o---------COMMON     "dotenv": "*"
 o---------COMMON   },
 o---------COMMON   "devDependencies": {
-o-----------JEST     "jest": "*"
-o----------MOCHA     "mocha": "*"
+o-----------JEST     "jest": "*",
+o----------MOCHA     "mocha": "*",
+o---------COMMON     "supertest": "*"
 o---------COMMON   }
 o---------COMMON }
 `
