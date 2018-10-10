936631dd93e9165cd4596d0cc62d0d0cfeb57410
Alex Pereira <alex.pereira@outlook.com>
2018-10-10 13:23:41 -0400

    Update database knex connection

--- src/factory/code/common/src/utils/database.ts
+++ src/factory/code/common/src/utils/database.ts
@@ -3,11 +3,14 @@ o-----------KNEX var knex = require('knex')
 o-----------KNEX 
 o-----------KNEX // Pooling connections: https://www.npmjs.com/package/mysql#pooling-connections
 o-----------KNEX var database = knex({
-o-----------KNEX   host:     process.env.DATABASE_HOST || '0.0.0.0',
-o-----------KNEX   port:     process.env.DATABASE_PORT || '3306',
-o-----------KNEX   database: process.env.DATABASE_NAME
-o-----------KNEX   user:     process.env.DATABASE_USER || 'root',
-o-----------KNEX   password: process.env.DATABASE_PASS,
+o-----------KNEX   client: 'mysql',
+o-----------KNEX   connection: {
+o-----------KNEX     host:     process.env.DATABASE_HOST || '0.0.0.0',
+o-----------KNEX     port:     process.env.DATABASE_PORT || '3306',
+o-----------KNEX     database: process.env.DATABASE_NAME,
+o-----------KNEX     user:     process.env.DATABASE_USER || 'root',
+o-----------KNEX     password: process.env.DATABASE_PASS
+o-----------KNEX   }
 o-----------KNEX })
 o-----------KNEX 
 o--------MONGODB var mongodb = require('mongodb')
