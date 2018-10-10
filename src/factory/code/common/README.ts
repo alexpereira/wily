61ef628d5c1206d0d7fbe4f046ffceaba18c8979
Alex Pereira <alex.pereira@outlook.com>
2018-10-10 13:31:39 -0400

    Update README

--- src/factory/code/common/README.ts
+++ src/factory/code/common/README.ts
@@ -5,7 +5,21 @@ o---------COMMON ## Getting Started
 o---------COMMON 
 o---------COMMON ## Prerequisites
 o---------COMMON 
-o---------COMMON Enter all environment variables in \`[project-path]/.env\` (This file is hidden by default)
+o---------COMMON * Access to a database (MySQL or MongoDB)
+o---------COMMON * Enter all environment variables in \`[project-path]/.env\` (This file is hidden by default). Example:
+o---------COMMON 
+o---------COMMON \`\`\`
+o---------COMMON # Server options
+o---------COMMON SERVER_HOST=localhost
+o---------COMMON SERVER_PORT=3000
+o---------COMMON 
+o---------COMMON # Database credentials
+o---------COMMON DATABASE_HOST=localhost
+o---------COMMON DATABASE_PORT=3306
+o---------COMMON DATABASE_NAME=top_secret
+o---------COMMON DATABASE_USER=admin
+o---------COMMON DATABASE_PASS=password
+o---------COMMON \`\`\`
 o---------COMMON 
 o---------COMMON ### Starting server
 o---------COMMON 
