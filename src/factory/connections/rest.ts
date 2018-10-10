e8cc530a7c0488587008ab6104901c8376a31d63
Alex Pereira <alex.pereira@outlook.com>
2018-10-10 12:19:27 -0400

    Update rest connection

--- src/factory/connections/rest.ts
+++ src/factory/connections/rest.ts
@@ -8,39 +8,43 @@ export = [
     to: '/package.json'
   },
   {
-    from: require('../code/common/indexFile'),
-    to: '/index.js'
+    from: require('../code/common/dotenv'),
+    to: '/.env'
   },
   {
-    from: require('../code/common/app'),
+    from: require('../code/common/server'),
+    to: '/server.js'
+  },
+  {
+    from: require('../code/common/src/app'),
     to: '/src/app.js'
   },
   {
-    from: require('../code/rest/controllers/user'),
+    from: require('../code/rest/src/controllers/user'),
     to: '/src/controllers/user.js'
   },
   {
-    from: require('../code/rest/models/user'),
+    from: require('../code/rest/src/models/user'),
     to: '/src/models/user.js'
   },
   {
-    from: require('../code/rest/routes/user'),
+    from: require('../code/rest/src/routes/user'),
     to: '/src/routes/user.js'
   },
   {
-    from: require('../code/common/validation/user'),
+    from: require('../code/common/src/validation/user'),
     to: '/src/validation/user.js'
   },
   {
-    from: require('../code/common/middleware/authentication'),
+    from: require('../code/common/src/middleware/authentication'),
     to: '/src/middleware/authentication.js'
   },
   {
-    from: require('../code/common/utils/database'),
+    from: require('../code/common/src/utils/database'),
     to: '/src/utils/database.js'
   },
   {
-    from: require('../code/common/utils/logger'),
-    to: '/src/utils/logger.js'
+    from: require('../code/common/src/errors/InternalError'),
+    to: '/src/errors/InternalError.js'
   }
 ]
