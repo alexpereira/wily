d7625ad24579b79e6bf6b3842c04574eed69a054
Alex Pereira <alex.pereira@outlook.com>
2018-10-11 01:07:40 -0400

    Update controllers

--- src/factory/code/rest/src/controllers/user.ts
+++ src/factory/code/rest/src/controllers/user.ts
@@ -5,92 +5,136 @@ o---------COMMON
 o---------COMMON class UserController {
 o---------COMMON 
 o---------COMMON   constructor() {
+o---------COMMON     this.userModel = new UserModel()
+o---------COMMON 
+o---------COMMON     this.registerUser = this.registerUser.bind(this)
 o---------COMMON     this.loginUser = this.loginUser.bind(this)
-o---------COMMON     this.logoutUser = this.logoutUser.bind(this)
 o---------COMMON     this.getUsers = this.getUsers.bind(this)
 o---------COMMON     this.getUser = this.getUser.bind(this)
-o---------COMMON     this.insertUser = this.insertUser.bind(this)
-o---------COMMON     this.updateUserFull = this.updateUserFull.bind(this)
-o---------COMMON     this.updateUserPartial = this.updateUserPartial.bind(this)
+o---------COMMON     this.updateUser = this.updateUser.bind(this)
 o---------COMMON     this.deleteUser = this.deleteUser.bind(this)
 o---------COMMON   }
 o---------COMMON 
-o---------COMMON   loginUser(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateLogin(body)
+o---------COMMON   async registerUser(req, res) {
+o---------COMMON     var { body: user } = req
 o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
-o---------COMMON 
-o---------COMMON     // assign jwt
-o---------COMMON     var jwt = 'cool'
-o---------COMMON     res.status(200).send(jwt)
-o---------COMMON   }
-o---------COMMON 
-o---------COMMON   logoutUser(req, res) {
-o---------COMMON     req.logout()
-o---------COMMON     res.status(200).send()
-o---------COMMON   }
-o---------COMMON 
-o---------COMMON   async getUsers(req, res) {
-o---------COMMON     var userModel = new UserModel()
-o---------COMMON     var users = []
 o---------COMMON     try {
-o---------COMMON       users = await userModel.getUsers()
+o---------COMMON       userValidation.validateRegister(user)
 o---------COMMON     } catch (error) {
 o---------COMMON       res.status(error.status).send(error.message)
 o---------COMMON       return
 o---------COMMON     }
+o---------COMMON 
+o---------COMMON     try {
+o---------COMMON       user = await this.userModel.insertUser(user)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
+o---------COMMON     res.status(200).send(user)
+o---------COMMON   }
+o---------COMMON 
+o---------COMMON   async loginUser(req, res) {
+o---------COMMON     var { body: user } = req
+o---------COMMON 
+o---------COMMON     try {
+o---------COMMON       userValidation.validateLogin(user)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
+o---------COMMON     var token = ''
+o---------COMMON     try {
+o---------COMMON       token = await this.userModel.authenticateUser(user)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
+o---------COMMON     res.status(200).send({ token })
+o---------COMMON   }
+o---------COMMON 
+o---------COMMON   async getUsers(req, res) {
+o---------COMMON     var users = []
+o---------COMMON     try {
+o---------COMMON       users = await this.userModel.getUsers()
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
 o---------COMMON     res.status(200).send(users)
 o---------COMMON   }
 o---------COMMON 
-o---------COMMON   getUser(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateUsername(body)
+o---------COMMON   async getUser(req, res) {
+o---------COMMON     var { username } = req.params
 o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
+o---------COMMON     try {
+o---------COMMON       userValidation.validateUsername(username)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
 o---------COMMON 
-o---------COMMON     var user = new UserModel().getUser()
+o---------COMMON     var user = {}
+o---------COMMON     try {
+o---------COMMON       user = await this.userModel.getUser(username)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
 o---------COMMON     res.status(200).send(user)
 o---------COMMON   }
 o---------COMMON 
-o---------COMMON   insertUser(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateInsertUser(body)
+o---------COMMON   async updateUser(req, res) {
+o---------COMMON     var { method, body: user, params: { username } } = req
 o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
+o---------COMMON     try {
+o---------COMMON       user = userValidation.validateUser(user)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
 o---------COMMON 
-o---------COMMON     var user = new UserModel().insertUser(body)
+o---------COMMON     if (method == 'PATCH') {
+o---------COMMON       for (var field in user) { 
+o---------COMMON         if (user[field] === null) {
+o---------COMMON           delete user[field]
+o---------COMMON         }
+o---------COMMON       }
+o---------COMMON     }
+o---------COMMON 
+o---------COMMON     try {
+o---------COMMON       user = await this.userModel.updateUser(username, user)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
+o---------COMMON 
 o---------COMMON     res.status(200).send(user)
 o---------COMMON   }
 o---------COMMON 
-o---------COMMON   updateUserFull(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateUpdateUserFull(body)
+o---------COMMON   async deleteUser(req, res) {
+o---------COMMON     var { username } = req.params
 o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
+o---------COMMON     try {
+o---------COMMON       userValidation.validateUsername(username)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
 o---------COMMON 
-o---------COMMON     var user = new UserModel().updateUserFull(body)
-o---------COMMON     res.status(200).send(user)
-o---------COMMON   }
+o---------COMMON     try {
+o---------COMMON       await this.userModel.deleteUser(username)
+o---------COMMON     } catch (error) {
+o---------COMMON       res.status(error.status).send(error.message)
+o---------COMMON       return
+o---------COMMON     }
 o---------COMMON 
-o---------COMMON   updateUserPartial(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateUpdateUserPartial(body)
-o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
-o---------COMMON 
-o---------COMMON     var user = new UserModel().updateUserPartial(body)
-o---------COMMON     res.status(200).send(user)
-o---------COMMON   }
-o---------COMMON 
-o---------COMMON   deleteUser(req, res) {
-o---------COMMON     var { body } = req
-o---------COMMON     var valid = userValidation.validateUsername(body)
-o---------COMMON 
-o---------COMMON     if (!valid) throw new Error('INVALID!')
-o---------COMMON 
-o---------COMMON     var user = new UserModel().deleteUser()
-o---------COMMON     res.status(200).send(user)
+o---------COMMON     res.status(200).send({})
 o---------COMMON   }
 o---------COMMON }
 o---------COMMON 
