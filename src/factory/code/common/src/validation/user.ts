600d11013619b9461662594be7ad9de88cddd482
Alex Pereira <alex.pereira@outlook.com>
2018-10-11 01:06:51 -0400

    Update validation

--- src/factory/code/common/src/validation/user.ts
+++ src/factory/code/common/src/validation/user.ts
@@ -1,37 +1,58 @@
 export = `
-o------------JOI var joi = require('joi')
+o------------JOI var Joi = require('joi')
 o------VALIDATOR var validator = require('validator')
+o---------COMMON var BadRequest = require('../errors/BadRequest')
+o---------COMMON 
+o---------COMMON function validateRegister(user) {
+o---------COMMON   try {
+o------------JOI     Joi.attempt(user, Joi.object({
+o------------JOI       username: Joi.string().required(),
+o------------JOI       password: Joi.string().required()
+o------------JOI     }))
+o---------COMMON   } catch (error) {
+o---------COMMON     console.error(error.details)
+o---------COMMON     throw new BadRequest()
+o---------COMMON   }
+o---------COMMON }
 o---------COMMON 
 o---------COMMON function validateLogin(user) {
-o------------JOI   return joi.validate(user)
-o------VALIDATOR   return validator.validate(user)
+o---------COMMON   try {
+o------------JOI     Joi.attempt(user, Joi.object({
+o------------JOI       username: Joi.string().required(),
+o------------JOI       password: Joi.string().required()
+o------------JOI     }))
+o---------COMMON   } catch (error) {
+o---------COMMON     console.error(error.details)
+o---------COMMON     throw new BadRequest()
+o---------COMMON   }
 o---------COMMON }
 o---------COMMON 
-o---------COMMON function validateUsername(user) {
-o------------JOI   return joi.validate(user)
-o------VALIDATOR   return validator.validate(user)
+o---------COMMON function validateUsername(username) {
+o---------COMMON   try {
+o------------JOI     Joi.attempt(username, Joi.string())
+o---------COMMON   } catch (error) {
+o---------COMMON     console.error(error.details)
+o---------COMMON     throw new BadRequest()
+o---------COMMON   }
 o---------COMMON }
 o---------COMMON 
-o---------COMMON function validateInsertUser(user) {
-o------------JOI   return joi.validate(user)
-o------VALIDATOR   return validator.validate(user)
-o---------COMMON }
-o---------COMMON 
-o---------COMMON function validateUpdateUserFull(user) {
-o------------JOI   return joi.validate(user)
-o------VALIDATOR   return validator.validate(user)
-o---------COMMON }
-o---------COMMON 
-o---------COMMON function validateUpdateUserPartial(user) {
-o------------JOI   return joi.validate(user)
-o------VALIDATOR   return validator.validate(user)
+o---------COMMON function validateUser(user) {
+o---------COMMON   try {
+o------------JOI     user = Joi.validate(user, Joi.object({
+o------------JOI       username: Joi.string().default(null),
+o------------JOI       password: Joi.string().default(null)
+o------------JOI     }), (_, value) => value)
+o---------COMMON   } catch (error) {
+o---------COMMON     console.error(error.details)
+o---------COMMON     throw new BadRequest()
+o---------COMMON   }
+o---------COMMON   return user
 o---------COMMON }
 o---------COMMON 
 o---------COMMON module.exports = {
+o---------COMMON   validateUser,
 o---------COMMON   validateLogin,
-o---------COMMON   validateUsername,
-o---------COMMON   validateInsertUser,
-o---------COMMON   validateUpdateUserFull,
-o---------COMMON   validateUpdateUserPartial
+o---------COMMON   validateRegister,
+o---------COMMON   validateUsername
 o---------COMMON }
 `
