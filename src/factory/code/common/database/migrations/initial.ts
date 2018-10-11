ace9e37e7e37b8122852ae492c36c56121c9185b
Alex Pereira <alex.pereira@outlook.com>
2018-10-11 01:06:11 -0400

    Update migration

--- src/factory/code/common/database/migrations/initial.ts
+++ src/factory/code/common/database/migrations/initial.ts
@@ -3,9 +3,9 @@ o-----------KNEX exports.up = function(knex, Promise) {
 o-----------KNEX   return Promise.all([
 o-----------KNEX     knex.schema.createTable('users', function (table) {
 o-----------KNEX       table.increments()
-o-----------KNEX       table.string('username')
+o-----------KNEX       table.string('username').unique()
 o-----------KNEX       table.string('password')
-o-----------KNEX       table.timestamps()
+o-----------KNEX       table.timestamps(false, true)
 o-----------KNEX     })
 o-----------KNEX   ])
 o-----------KNEX }
