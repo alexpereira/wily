116f51956e3bc27c2aca29bba202fb854145c2ad
Alex Pereira <alex.pereira@outlook.com>
2018-10-11 01:07:17 -0400

    Update routes

--- src/factory/code/rest/src/routes/user.ts
+++ src/factory/code/rest/src/routes/user.ts
@@ -1,20 +1,20 @@
 export = `
 o--------EXPRESS var { Router } = require('express')
 o--------EXPRESS 
+o------------JWT var verifyUser = require('../middleware/verifyUser')
 o---------COMMON var UserController = require('../controllers/user')
 o---------COMMON var userController = new UserController()
 o---------COMMON 
 o--------EXPRESS var routes = new Router()
 o--------EXPRESS 
+o--------EXPRESS routes.post('/users', userController.registerUser)
 o--------EXPRESS routes.post('/login', userController.loginUser)
-o--------EXPRESS routes.post('/logout', userController.logoutUser)
 o--------EXPRESS 
-o--------EXPRESS routes.get('/users', userController.getUsers)
-o--------EXPRESS routes.get('/users/:username', userController.getUser)
-o--------EXPRESS routes.post('/users', userController.insertUser)
-o--------EXPRESS routes.put('/users/:username', userController.updateUserFull)
-o--------EXPRESS routes.patch('/users/:username', userController.updateUserPartial)
-o--------EXPRESS routes.delete('/users/:username', userController.deleteUser)
+o--------EXPRESS routes.get('/users', verifyUser, userController.getUsers)
+o--------EXPRESS routes.get('/users/:username', verifyUser, userController.getUser)
+o--------EXPRESS routes.put('/users/:username', verifyUser, userController.updateUser)
+o--------EXPRESS routes.patch('/users/:username', verifyUser, userController.updateUser)
+o--------EXPRESS routes.delete('/users/:username', verifyUser, userController.deleteUser)
 o--------EXPRESS 
 o-----------HAPI var routes = [
 o-----------HAPI  { method: 'POST', path: '/login', handler: userController.loginUser },
