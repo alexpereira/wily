29674e8bc5a1c16ce1ee700f7f02e257a0897266
Alex Pereira <alex.pereira@outlook.com>
2018-11-14 18:53:38 -0500

    Update READMEs and projectStack for v0.1.1

--- src/questions/projectStack.ts
+++ src/questions/projectStack.ts
@@ -4,67 +4,67 @@ export const projectStack = [
   {
     type: 'list',
     name: 'server',
-    message: 'Type of server: ',
-    choices: ['rest', 'graphql'],
+    message: 'Server: ',
+    choices: ['rest', /*'graphql'*/],
     default: 'rest'
   },
   {
     type: 'list',
     name: 'framework',
-    message: 'Type of framework: ',
+    message: 'Framework: ',
     choices: ['express', 'hapi'],
     default: 'express',
   },
   {
     type: 'list',
     name: 'database',
-    message: 'Type of database: ',
-    choices: ['mysql', 'mongodb'],
+    message: 'Database: ',
+    choices: ['mysql', /*'mongodb'*/],
     default: 'mysql'
   },
   {
     type: 'list',
     name: 'orm',
-    message: 'Type of ORM: ',
-    choices: ['knex', 'sequelize'],
+    message: 'ORM: ',
+    choices: ['knex', /*'sequelize'*/],
     default: 'knex',
     when: (answers: ProjectStack) => answers.database == 'mysql'
   },
-  {
+  /*{
     type: 'list',
-    name: 'orm',
-    message: 'Type of ORM: ',
-    choices: ['mongoose', 'native mongodb'],
+    name: 'odm',
+    message: 'ODM: ',
+    choices: ['mongoose', 'native-mongodb'],
     default: 'mongoose',
     when: (answers: ProjectStack) => answers.database == 'mongodb'
-  },
+  },*/
   {
     type: 'list',
     name: 'auth',
-    message: 'Type of authentication: ',
-    choices: ['jwt', 'express-session'],
+    message: 'Authentication: ',
+    choices: ['jwt', /*'express-session'*/],
     default: 'jwt',
     when: (answers: ProjectStack) => answers.framework == 'express'
   },
   {
     type: 'list',
     name: 'auth',
-    message: 'Type of authentication: ',
-    choices: ['jwt', 'yar'],
+    message: 'Authentication: ',
+    choices: ['jwt', /*'yar'*/],
     default: 'jwt',
     when: (answers: ProjectStack) => answers.framework == 'hapi'
   },
   {
     type: 'list',
     name: 'validation',
-    message: 'Type of validation: ',
-    choices: ['joi', 'validator'],
+    message: 'validation: ',
+    choices: ['joi', /*'validator'*/],
     default: 'joi'
   },
   {
     type: 'list',
     name: 'test',
-    message: 'Type of test: ',
+    message: 'test: ',
     choices: ['mocha', 'jest'],
     default: 'mocha'
   }
