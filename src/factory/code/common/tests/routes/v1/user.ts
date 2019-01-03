3fe92988d2cd161ec591aa7927bb418d145bc882
Alex Pereira <alex.pereira@outlook.com>
2019-01-03 16:23:57 -0500

    Refactor Jest timeout

--- src/factory/code/common/tests/routes/v1/user.ts
+++ src/factory/code/common/tests/routes/v1/user.ts
@@ -5,9 +5,9 @@ o---------COMMON describe('Testing API...', function () {
 o---------COMMON   var server = {}
 o----------MOCHA   before(function () {
 o-----------JEST   beforeAll(function () {
+o---------COMMON     jest.setTimeout(30000)
 o---------COMMON     server = require('../../../server')
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o---------COMMON   var testUser = { 
 o---------COMMON     username: 'test', 
@@ -27,8 +27,7 @@ o--------EXPRESS     request(server)
 o-----------HAPI     request(server.listener)
 o---------COMMON       .get('/v1/ping')
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[POST] /v1/users', function(done) {
 o-----------JEST   test('[POST] /v1/users', function(done) {
@@ -37,8 +36,7 @@ o-----------HAPI     request(server.listener)
 o---------COMMON       .post('/v1/users')
 o---------COMMON       .send(testUser)
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[POST] /v1/login', function(done) {
 o-----------JEST   test('[POST] /v1/login', function(done) {
@@ -48,8 +46,7 @@ o---------COMMON       .post('/v1/login')
 o---------COMMON       .send(testUser)
 o---------COMMON       .expect(res => token = res.body.token)
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[GET] /v1/users', function(done) {
 o-----------JEST   test('[GET] /v1/users', function(done) {
@@ -58,8 +55,7 @@ o-----------HAPI     request(server.listener)
 o---------COMMON       .get('/v1/users')
 o---------COMMON       .set('Authorization', \`Bearer \${token}\`)
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[GET] /v1/users/:username', function(done) {
 o-----------JEST   test('[GET] /v1/users/:username', function(done) {
@@ -68,8 +64,7 @@ o-----------HAPI     request(server.listener)
 o---------COMMON       .get(\`/v1/users/\${testUser.username}\`)
 o---------COMMON       .set('Authorization', \`Bearer \${token}\`)
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[PUT] /v1/users/:username', function(done) {
 o-----------JEST   test('[PUT] /v1/users/:username', function(done) {
@@ -79,8 +74,7 @@ o---------COMMON       .put(\`/v1/users/\${testUser.username}\`)
 o---------COMMON       .set('Authorization', \`Bearer \${token}\`)
 o---------COMMON       .send({ username: updatedUser.username })
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[PATCH] /v1/users/:username', function(done) {
 o-----------JEST   test('[PATCH] /v1/users/:username', function(done) {
@@ -90,8 +84,7 @@ o---------COMMON       .patch(\`/v1/users/\${updatedUser.username}\`)
 o---------COMMON       .set('Authorization', \`Bearer \${token}\`)
 o---------COMMON       .send({ password: updatedUser.password })
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[DELETE] /v1/users/:username', function(done) {
 o-----------JEST   test('[DELETE] /v1/users/:username', function(done) {
@@ -100,8 +93,7 @@ o-----------HAPI     request(server.listener)
 o---------COMMON       .delete(\`/v1/users/\${updatedUser.username}\`)
 o---------COMMON       .set('Authorization', \`Bearer \${token}\`)
 o---------COMMON       .expect(200, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[GET] /v1/users [UNAUTHORIZED]', function(done) {
 o-----------JEST   test('[GET] /v1/users [UNAUTHORIZED]', function(done) {
@@ -109,8 +101,7 @@ o--------EXPRESS     request(server)
 o-----------HAPI     request(server.listener)
 o---------COMMON       .get('/v1/users')
 o---------COMMON       .expect(401, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   it('[POST] /v1/login [BAD REQUEST]', function(done) {
 o-----------JEST   test('[POST] /v1/login [BAD REQUEST]', function(done) {
@@ -119,14 +110,12 @@ o-----------HAPI     request(server.listener)
 o---------COMMON       .post('/v1/login')
 o---------COMMON       .send({ username: 'test', password: false })
 o---------COMMON       .expect(400, done)
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON 
 o----------MOCHA   after(async function () {
 o-----------JEST   afterAll(async function () {
 o----------MOCHA     await server.shutdown({ exit: true })
 o-----------JEST     await server.shutdown({ cleanup: true })
-o----------MOCHA   })
-o-----------JEST   }, 10000)
+o---------COMMON   })
 o---------COMMON })
 o---------COMMON `
